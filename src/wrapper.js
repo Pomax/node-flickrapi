5a1699178abbe744e7d8751e6344fa92ffbca3c7
genffy <lee.genffy@gmail.com>
2016-06-24 10:58:24 +0800

    replace tab with 2-space

--- src/wrapper.js
+++ src/wrapper.js
@@ -1,23 +1,23 @@
 (function(global, factory) {
-	if (typeof module === "object" && typeof module.exports === "object") {
-		module.exports = factory(global);
-	} else {
-		factory(global);
-	}
-	// Pass this if window is not defined yet
+  if (typeof module === "object" && typeof module.exports === "object") {
+    module.exports = factory(global);
+  } else {
+    factory(global);
+  }
+  // Pass this if window is not defined yet
 }(typeof window !== "undefined" ? window : this, function(window, noGlobal) {
 
   //@CODE
 
-	// Register as a named AMD module
-	if (typeof define === "function" && define.amd) {
-		define("flickrapi", [], function() {
-			return Flickr;
-		});
-	}
+  // Register as a named AMD module
+  if (typeof define === "function" && define.amd) {
+    define("flickrapi", [], function() {
+      return Flickr;
+    });
+  }
 
-	if (!noGlobal) {
-		window.Flickr = Flickr;
-	}
-	return Flickr;
+  if (!noGlobal) {
+    window.Flickr = Flickr;
+  }
+  return Flickr;
 }));
