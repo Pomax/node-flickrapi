ecab3fa3bd9a4fe200dd53da7fccb82cbebbd812
= <=>
2016-08-30 06:24:28 +0600

    Removed 'glob' dependency

---
+++ src/glob.js
@@ -0,0 +1,29 @@
+var fs = require("fs"),
+  path = require("path");
+
+module.exports = function(root, pattern) {
+  function walk(dir, pattern) {
+    if (!fs.existsSync(dir)) {
+      return;
+    }
+
+    var list = fs.readdirSync(dir);
+    if (!list.length) {
+      return;
+    }
+    for (var i = 0, len = list.length; i < len; i++) {
+      var file = list[i],
+        fullpath = path.resolve(dir, file);
+      if (fs.statSync(fullpath).isDirectory()) {
+        walk(fullpath);
+        continue;
+      }
+      if (fullpath.contains(pattern)) {
+        files.push(fullpath);
+      }
+    }
+  }
+
+  var files = [];
+  return walk(root, pattern);
+}
