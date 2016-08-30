0a570a766f944b1761496b6d7dca5c4908ab0b29
= <=>
2016-08-30 06:11:35 +0600

    Added caching

---
+++ build-api.js
@@ -0,0 +1,19 @@
+var fs = require('fs'),
+  consolidated = {
+  'flickr.reflection.getMethods.json': JSON.parse(fs.readFileSync(__dirname + '/data/flickr/flickr.reflection.getMethods.json'))
+}
+
+var jsonFiles = fs.readdirSync('./data/flickr/methods')
+for(var i=0, len=jsonFiles.length; i<len; i++) {
+  var filename = jsonFiles[i],
+    lastDot = filename.lastIndexOf('.')
+  if(filename.substring(lastDot) !== '.json') {
+    continue
+  }
+
+  var value = fs.readFileSync(__dirname + '/data/flickr/methods/' + filename, 'utf8')
+  consolidated['methods/' + filename] = JSON.parse(value)
+}
+
+fs.writeFileSync('./built-api.js', 'module.exports = ' + JSON.stringify(consolidated, null, '\t'))
+console.log('build-api.js regenerated with ' + Object.keys(consolidated).length + ' api files consolidated')
